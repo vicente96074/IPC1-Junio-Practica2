afd9398d0e648d74d33203ac54d1f5c1d27ddac3
Augusto Vicente <augustovicente201831464@cunoc.edu.gt>
2021-06-13 17:28:51 -0600

    Agregar 3. Mostrar todas las peliculas al menu principal

--- src/IPC1JunioPractica2.java
+++ src/IPC1JunioPractica2.java
@@ -54,49 +54,61 @@ public class IPC1JunioPractica2{
 	
 	private void menu(){
 		int opcion;
-		
+		//Un hacer mientras para el menu principal
 		do{
 			System.out.println("\n -------- Menú -------- ");
 			System.out.print("1. Préstamo de películas"
 					  + "\n2. Devolución de películas"
 					  + "\n3. Mostrar las películas" 
 					  + "\n4. Ingreso de Películas"
-					  + "\n5. Ingresar clientes nuevos"
-					  + "\n6. Reportes"
-					  + "\n7. Salir"
+					  + "\n5. Ordenar peliculas"
+					  + "\n6. Ingresar clientes nuevos"
+					  + "\n7. Mostrar clientes"
+					  + "\n8. Reportes"
+					  + "\n9. Salir"
 					  + "\nIngrese una opcion entre (1-6): ");		
 			opcion = entrada.nextInt();
 			
+			//Swicth que controla al menu principal
 			switch(opcion){
 				case 1:
-					//Mostramos la opcion prestamo de peliculas
 					prestamoDePeliculas();
 					break;
 				case 2:
+					devoluciónDePeliculas();
 					break;
 				case 3:
+					mostrarLasPeliculas();
 					break;
 				case 4:
+					ingresoDePeliculas();
 					break;
 				case 5:
-					ingresarClientesNuevos();
+					ordenarLasPeliculas();
 					break;
 				case 6:
+					ingresarClientesNuevos();
 					break;
 				case 7:
+					mostrarClientes();
+					break;
+				case 8:
+					reportes();
+					break;
+				case 9:
 					System.out.println("\n¡Saliendo del programa!");
 					break;
 				default:
 					System.out.println("\n!Opción inválida, vuela a digitar!");
 			}
-		}while(opcion!=7);
+		}while(opcion!=9);
 	}
 	
-	
+	//Funcion para el prestamo de las peliculas
 	private void prestamoDePeliculas(){
 		//Variables locales
 		boolean encontrado = false;
-		int posCliente = 1;
+		int posCliente = 0;
 		int idCl;
 		int idP;
 		
@@ -105,6 +117,7 @@ public class IPC1JunioPractica2{
 		System.out.print("Ingrese su id, si no tiene id crea uno en el menu principal: ");
 		idCl = entrada.nextInt();
 		
+		//Buscar el id del cliente en el arreglo idCliente[]
 		for(int i=0; i<idCliente.length; i++){
 			if(idCl == idCliente[i]){
 				posCliente = i;
@@ -125,13 +138,14 @@ public class IPC1JunioPractica2{
 			}
 			//Comprobar que el cliente aun no tenga prestado una pelicula
 			if(tienePeliculaPrestado[posCliente] == false){
-				System.out.println("Seleccione una pelicula ingresando el id de la pelicula: ");
+				System.out.print("\nSeleccione una pelicula ingresando el id de la pelicula: ");
 				idP = entrada.nextInt();
-				int posEncontrado = 0;
+				int posPelicula = 0;
+				
 				//Comprobar que exista la pelicula
 				for(int i=0; i<idPelicula.length; i++){
 					if(idP==idPelicula[i]){
-						posEncontrado = i;
+						posPelicula = i;
 						encontrado = true;
 						break;
 					} else {
@@ -139,8 +153,9 @@ public class IPC1JunioPractica2{
 					}
 				}
 				
+				//Llamamos a la funcion que archive a las peliculas prestadas
 				if(encontrado == true){
-					disponiblePelicula[posEncontrado] = false;
+					tablaPrestamos(posCliente, posPelicula);
 				} else {
 					System.out.println("¡Ingresó un id de pelicula inexistente!");
 				}
@@ -155,22 +170,127 @@ public class IPC1JunioPractica2{
 		}
 	}
 	
-	
-	private void ingresarClientesNuevos(){
-	/*
-		System.out.println("Ingrese su nombre: ");
-		nombreCliente[posicionCliente] = entrada.nextLine();
-		System.out.println("Ingrese su id: ");
-		idCliente[posicionCliente] = entrada.nextInt();
-		System.out.println("Ingrese su telefono: ");
-		telefonoCliente[posicionCliente] = entrada.nextInt();
-	*/
+	private void tablaPrestamos(int posCliente, int posPelicula){
+		int opcion;
+		do{
+			System.out.println("\nLa pelicula se guardara en al tabla préstamo");
+			System.out.print("1. Continuar"
+					  + "\n2. Cancelar"
+					  + "\nDigite una opcion: ");
+			opcion = entrada.nextInt();
+								
+			switch(opcion){
+				case 1:
+					int dia;
+					boolean continuar = false;
+					//Pedir al usuario los días que quiere prestar una pelicula
+					do{
+						
+						System.out.print("\nCantidad de días disponibles para prestar una pelicula:"
+								  + "\n1. 1 día"
+								  + "\n2. 5 días"
+								  + "\n3. 15 días"
+								  + "\n4. 30 días"
+								  + "\nSeleccione una opción: ");
+						dia = entrada.nextInt();
+						if(dia>=1 && dia<=4){
+							continuar = true;
+						} else {
+							System.out.println("¡Opción incorrecta, vuelva a digitar!");
+							continuar = false;
+						}
+						
+					}while(continuar == false);
+					
+					//Hacemos los cambios a los estados del cliente y la pelicula
+					tienePeliculaPrestado[posCliente] = true;
+					disponiblePelicula[posPelicula] = false;
+					idClientePrestador[peliculasPrestados-1] = idCliente[posCliente];
+					idPeliculaPrestado[peliculasPrestados-1] = idPelicula[posPelicula];
+					
+					//Agregamos los días que se presta una pelicula
+					switch(dia){
+						case 1:
+							diaPrestadoPelicula[peliculasPrestados-1] = 1;
+							break;
+						case 2:
+							diaPrestadoPelicula[peliculasPrestados-1] = 5;
+							break;
+						case 3:
+							diaPrestadoPelicula[peliculasPrestados-1] = 10;
+							break;
+						case 4:
+							diaPrestadoPelicula[peliculasPrestados-1] = 30;
+							break;
+					}
+					System.out.println("\n¡Pelicula prestado exitosamente!");
+					break;
+				case 2:
+					System.out.println("¡Regresando!");
+					break;
+				default:
+					System.out.println("\n¡Opción inválida, vuelva a digitar!");
+			}
+		}while(opcion!=1 && opcion!=2);
 	}
 	
+	//Funcion para devolver las peliculas
+	private void devoluciónDePeliculas(){
+		
+		
+	}
+	
+	//Funcion para mostrar todas las peliculas
+	private void mostrarLasPeliculas(){
+		System.out.println("\nTodas las peliculas");
+			for(int i=0; i<idPelicula.length; i++){
+				System.out.println("Id: "+idPelicula[i]+" --  nombre: " + nombrePelicula[i] +
+				" --  año: " +añoPelicula[i] + " --  categoria: "+categoriaDeCadaPelicula[i]+
+				" --  disponible: "+ disponiblePelicula[i]);
+			}
+	}
+	
+	//Funcion Ingreso de Películas
+	private void ingresoDePeliculas(){
+		
+		
+	}
+	
+	//Funcion ordenar peliculas respecto al id
+	private void ordenarLasPeliculas(){
+		
+		
+	}
+	
+	//Funcion ingresar clientes nuevos
+	private void ingresarClientesNuevos(){
+		/*System.out.println("Ingrese su nombre: ");
+		nombreCliente[nClientes] = entrada.nextLine();
+		System.out.println("Ingrese su id: ");
+		idCliente[nClientes] = entrada.nextInt();
+		System.out.println("Ingrese su telefono: ");
+		telefonoCliente[nClientes] = entrada.nextInt();
+		*/
+	}
+	
+	//Funcion para mostrar clientes
+	private void mostrarClientes(){
+		
+		
+	}
+	
+	//Funcion para Reportes	
+	private void reportes(){
+		
+		
+	}
+	
+	//Metodo principal
 	public static void main(String[] args){
 		IPC1JunioPractica2 iPC1JunioPractica2 = new IPC1JunioPractica2();
 	}
 	
+	//Variables y el scanner
 	Scanner entrada = new Scanner(System.in);
 	int nPeliculas = 5;
 	int nClientes = 1;
@@ -190,6 +310,10 @@ public class IPC1JunioPractica2{
 	boolean[] disponiblePelicula = new boolean[nPeliculas];
 	
 	//Arreglo para los dias  de prestado para cada pelicula
+	int peliculasPrestados = 1;
 	int[] díasPrestamo = {1,5,15,30};
-	
+	int[] idClientePrestador  = new int[peliculasPrestados];
+	int[] idPeliculaPrestado  = new int[peliculasPrestados];
+	int[] diaPrestadoPelicula = new int[peliculasPrestados];
+	int[] 
 }
