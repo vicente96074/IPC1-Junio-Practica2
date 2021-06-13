21a915f82e8b1454b10753b856e9f65c839692e8
Augusto Vicente <augustovicente201831464@cunoc.edu.gt>
2021-06-12 20:56:04 -0600

    Crear proyecto IPC1JunioPractica2.java

---
+++ src/IPC1JunioPractica2.java
@@ -0,0 +1,18 @@
+package src;
+import java.util.Scanner;
+public class IPC1JunioPractica2{
+	
+	public IPC1JunioPractica2(){
+		iniciarComponentes();
+	}
+	
+	private void iniciarComponentes(){
+		System.out.println("Menú");
+	}
+	
+	public static void main(String[] args){
+		IPC1JunioPractica2 iPC1JunioPractica2 = new IPC1JunioPractica2();
+	}
+	
+	
+}
